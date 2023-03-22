ad42528749911eebc0f96b3f09c509e90f3483a7
Nicolas_SuRo <78867142+NicolasSuRo-Nivelics@users.noreply.github.com>
2023-03-21 21:40:22 -0500

    feat: added header, footer and loading spinner for cards

--- src/app/components/organism/pokemon-table/pokemon-table.module.ts
+++ src/app/components/organism/pokemon-table/pokemon-table.module.ts
@@ -5,15 +5,27 @@ import { PokemonTableComponent } from './pokemon-table.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { PokemonCardComponent } from '../../molecules/pokemon-card/pokemon-card.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { TagTypeComponent } from '@atoms/tag-type/tag-type.component';
+import { HeaderComponent } from '@molecules/header/header.component';
+import { FooterComponent } from '@molecules/footer/footer.component';
 
 @NgModule({
-  declarations: [PokemonTableComponent, PokemonCardComponent, TagTypeComponent],
+  declarations: [
+    PokemonTableComponent,
+    PokemonCardComponent,
+    TagTypeComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   imports: [
     CommonModule,
     HttpClientModule,
     PokemonTableRoutingModule,
     MatCardModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
   ],
   providers: [HttpClient, HttpClientModule],
 })
